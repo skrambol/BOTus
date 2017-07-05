e8c8985af5c4c7b08d3e81cdeba7852258649b03
giooooo <gbperalta1@up.edu.ph>
2017-07-05 11:40:18 +0800

    Added dem modafakin lodash

--- src/api/randomizer/randomizer.class.js
+++ src/api/randomizer/randomizer.class.js
@@ -1,9 +1,12 @@
 
+import _ from 'lodash';
+
 const log = require('debug')('randomizer');
 
 export default class Randomizer {
   static randomize(n, array) {
     log('Randomizing dem Araizes');
     log(n, array);
+    _.shuffle(array);
   }
 }
